82673c2cc7f21af26419988d01c303c543c68f44
Jerry Kasper <Jerry@kasperfam.net>
2018-11-28 20:45:38 -0800

    asdf

--- new.ps1
+++ new.ps1
@@ -8,7 +8,8 @@ Process {
     $gatewayObject = Get-ADComputer -Identity $gateway
     $nodeObject = Get-ADComputer -Identity $node
     Set-ADComputer -Identity $nodeObject -PrincipalsAllowedToDelegateToAccount $gatewayObject
+
 }
 End {
-    
+
 }
